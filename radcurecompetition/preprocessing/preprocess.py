8d9e97e5ec365c796349b5e583b3debbf0e00ca1
mkazmier <mkazmierski.poznan@gmail.com>
2020-04-21 16:23:46 -0400

    implemented preprocessing for clinical data

--- radcurecompetition/preprocessing/preprocess.py
+++ radcurecompetition/preprocessing/preprocess.py
@@ -1,10 +1,12 @@
 import os
+import glob
 from argparse import ArgumentParser
 
+import numpy as np
 import pandas as pd
 
 from imgtools.pipeline import Pipeline
-from imgtools.ops import ImageFileInput, ImageFileOutput, MetadataOutput, StructureSetToSegmentation
+from imgtools.ops import ImageCSVInput, ImageFileOutput, MetadataOutput, StructureSetToSegmentation
 from imgtools.io import read_dicom_series, read_dicom_rtstruct
 
 
@@ -12,7 +14,7 @@ class RadcurePipeline(Pipeline):
     def __init__(self,
                  input_directory,
                  output_directory,
-                 id_mapping_path,
+                 clinical_data_path,
                  roi_names=["GTV"],
                  train_size=.7,
                  save_splits_path="../../data/splits.csv",
@@ -23,21 +25,30 @@ class RadcurePipeline(Pipeline):
 
         self.input_directory = input_directory
         self.output_directory = output_directory
-        self.id_mapping_path = id_mapping_path
+        self.clinical_data_path = clinical_data_path
         self.roi_names = roi_names
         self.train_size = train_size
         self.save_splits_path = save_splits_path
 
-        self.image_input = ImageFileInput(
-            self.input_directory,
-            get_subject_id_from="subject_directory",
-            subdir_path="*/ImageSet_*",
-            reader=read_dicom_series)
-        self.structure_set_input = ImageFileInput(
-            self.input_directory,
-            get_subject_id_from="subject_directory",
-            subdir_path="*/structures/RTSTRUCT.dcm",
-            reader=read_dicom_rtstruct)
+        self.clinical_data = self.load_clinical_data()
+        if self.save_splits_path:
+            self.clinical_data.drop("MRN", axis=1).to_csv(self.save_splits_path, index=False)
+
+        # self.image_input = ImageFileInput(
+        #     self.input_directory,
+        #     get_subject_id_from="subject_directory",
+        #     subdir_path="*/ImageSet_*",
+        #     reader=read_dicom_series)
+        # self.structure_set_input = ImageFileInput(
+        #     self.input_directory,
+        #     get_subject_id_from="subject_directory",
+        #     subdir_path="*/structures/RTSTRUCT.dcm",
+        #     reader=read_dicom_rtstruct)
+
+        self.image_input = ImageCSVInput(self.clinical_data,
+                                         colnames=["image_path", "rtstruct_path"],
+                                         id_column="Study ID",
+                                         readers=[read_dicom_series, read_dicom_rtstruct])
 
         self.make_binary_mask = StructureSetToSegmentation(
             roi_names=self.roi_names)
@@ -53,34 +64,101 @@ class RadcurePipeline(Pipeline):
             create_dirs=True,
             compress=True)
 
-        id_mapping = pd.read_csv(self.id_mapping_path,
-                                 usecols=["MRN", "Study ID", "Date of dx"],
-                                 index_col="MRN")
-        self.id_mapping = self.split_by_date(id_mapping)
-        if self.save_splits_path:
-            self.id_mapping[["Study ID", "split"]].to_csv(self.save_splits_path, index=False)
+        self.clinical_data = self.clinical_data.set_index("MRN")
+
+    def load_clinical_data(self):
+        data = pd.read_csv(self.clinical_data_path)
+        images = os.listdir(self.input_directory)
+        rtstructs = [i for i in images if glob.glob(os.path.join(self.input_directory, i, "*", "*", "RTSTRUCT*"))]
+
+        sites = [
+            "Oropharynx",
+            "Larynx",
+            "Nasopharynx",
+            "Hypopharynx",
+            "Lip & Oral Cavity",
+            "Paranasal Sinus",
+            "nasal cavity",
+            "Nasal Cavity",
+            "esophagus",
+            "Esophagus",
+            "Salivary Glands"
+        ]
+
+        data["has_image"] = (data["MRN"].isin(images)) & (data["MRN"].isin(rtstructs))
+
+        data["image_path"] = [glob.glob(os.path.join(self.input_directory, mrn, "*", "ImageSet*")) for mrn in data["MRN"]]
+        data["rtstruct_path"] = [glob.glob(os.path.join(self.input_directory, mrn, "*", "structures", "RTSTRUCT*")) for mrn in data["MRN"]]
+
+        # exclusion criteria:
+        # - missing image or RTSTRUCT
+        # - site other than oro/naso/hypopharynx or oral cavity
+        # - < 2 yr follow-up
+        # - M stage > 0
+        # - prior surgery
+        data = data[(
+            data["has_image"] &
+            data["Disease Site"].isin(sites) &
+            ((data["Length FU"] >= 2) | (data["Status Last Follow Up"] == "Dead")) &
+            (data["M Stage"] == "M0") &
+            (data["Tx Intent"] != "post-op") &
+            ~(data["Date of death"].isnull())
+        )]
+
+        data = self.split_by_date(data)
+
+        # create target columns
+        data["target_binary"] = np.uint8((data["Length FU"] <= 2) & (data["Status Last Follow Up"] == "Dead"))
+        data["survival_time"] = data["Length FU"]
+        data["death"] = data["Status Last Follow Up"].map({"Dead": 1, "Alive": 0})
+
+        # clean up the clinical variables for phase II
+        data["Chemotherapy"] = data["Chemotherapy"].map(lambda x: 0 if "none" in x.lower() else 1, na_action="ignore")
+        data["Disease Site"] = data["Disease Site"].str.lower()
+        data["HPV Combined"] = data["HPV Combined"].map({"Yes, positive": 1, "Yes, Negative": 0})
+
+        data = data[[
+            "MRN",
+            "Study ID",
+            "image_path",
+            "rtstruct_path",
+            "target_binary",
+            "survival_time",
+            "death",
+            "age at dx",
+            "sex",
+            "T stage",
+            "N stage",
+            "Stage",
+            "Dose",
+            "Chemotherapy",
+            "HPV Combined",
+            "Disease Site"
+        ]]
+
+        return data
 
 
-    def split_by_date(self, id_mapping):
-        id_mapping["Date of dx"] = pd.to_datetime(id_mapping["Date of dx"])
-        cases_per_month = id_mapping.set_index("Date of dx").groupby(pd.Grouper(freq="M")).size()
+    def split_by_date(self, data):
+        data["Date of dx"] = pd.to_datetime(data["Date of dx"])
+        cases_per_month = data.set_index("Date of dx").groupby(pd.Grouper(freq="M")).size()
 
         # compute the cumulative % of total cases in each month
         cumulative_freqs = cases_per_month.cumsum() / cases_per_month.sum()
         # find the lowest date with >= train_size % data
         split_date = cumulative_freqs[cumulative_freqs >= self.train_size].index[0]
 
-        id_mapping["split"] = np.where(id_mapping["Date of dx"] < split_date, "train", "test")
+        data["split"] = np.where(data["Date of dx"] < split_date, "train", "test")
 
-        return id_mapping
+        return data
 
 
     def process_one_subject(self, subject_id):
-        image, rtstruct = self.image_input(subject_id), self.image_input(subject_id)
+        image, rtstruct = self.image_input(subject_id)
         mask = self.make_binary_mask(rtstruct, image)
 
-        new_subject_id = self.id_mapping.loc[subject_id, "Study ID"]
-        split = self.id_mapping.loc[subject_id, "split"]
+        new_subject_id = self.clinical_data.loc[subject_id, "Study ID"]
+        split = self.clinical_data.loc[subject_id, "split"]
         self.image_output(new_subject_id, image, split=split)
         self.mask_output(new_subject_id, mask, split=split)
 
