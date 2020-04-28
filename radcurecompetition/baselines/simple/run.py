d8d152101b4da4e48b7308ad99942466707497ee
mkazmier <mkazmierski.poznan@gmail.com>
2020-04-27 23:24:24 -0400

    clean up staging info before running baseline models

--- radcurecompetition/baselines/simple/run.py
+++ radcurecompetition/baselines/simple/run.py
@@ -19,14 +19,41 @@ def main(args):
     ]
     radiomics_columns = "firstorder|shape|glcm|glszm|glrlm|gldm|ngtdm"
 
+    clinical = pd.read_csv(args.clinical_data_path)
+    clinical["T Stage"] = clinical["T stage"].map({
+        "T1": "T1/2",
+        "T1a": "T1/2",
+        "T1b": "T1/2",
+        "T2": "T1/2",
+        "T2 (2)": "T1/2",
+        "T3": "T3/4",
+        "T3 (2)": "T3/4",
+        "T4": "T3/4",
+        "T4a": "T3/4",
+        "T4b": "T3/4"
+    })
+    clinical["N Stage"] = clinical["N stage"].map({
+        "N0": "N0",
+        "N1": "N1",
+        "N2": "N2",
+        "N2a": "N2",
+        "N2b": "N2",
+        "N2c": "N2",
+        "N3": "N3",
+        "N3a": "N3",
+        "N3b": "N3"
+    })
+
+    radiomics = pd.read_csv(args.radiomics_data_path)
+
     baselines = {
-        "clinical_baseline": SimpleBaseline(args.clinical_data_path,
+        "clinical_baseline": SimpleBaseline(clinical,
                                             max_features_to_select=0,
                                             colnames=clinical_columns),
-        "radiomics_baseline": SimpleBaseline(args.radiomics_data_path,
+        "radiomics_baseline": SimpleBaseline(radiomics,
                                              max_features_to_select=args.max_features_to_select,
                                              colnames=radiomics_columns),
-        "volume_baseline": SimpleBaseline(args.radiomics_data_path,
+        "volume_baseline": SimpleBaseline(radiomics,
                                           max_features_to_select=0,
                                           colnames=["original_shape_MeshVolume"])
     }
@@ -60,6 +87,10 @@ if __name__ == "__main__":
                         help="Path where the results will be saved.")
     parser.add_argument("--max_features_to_select", type=int, default=10,
                         help="Maximum number of features in the radiomics model.")
+    parser.add_argument("--n_permutations", type=int, default=5000,
+                        help="How many random permutations to use when evaluating significance.")
+    parser.add_argument("--n_jobs", type=int, default=1,
+                        help="Number of parallel processes to use.")
 
     args = parser.parse_args()
     main(args)
