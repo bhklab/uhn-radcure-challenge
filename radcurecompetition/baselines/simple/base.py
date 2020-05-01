fixed p value computation for evaluation metrics

radcurecompetition/baselines/simple/base.py
@@ -101,7 +101,7 @@ class BinaryModel:
         #                                   OneHotEncoder(drop="first", sparse=False),
         #                                   make_column_selector(dtype_include=object))])
         transformer = ColumnTransformer([('scale', StandardScaler(),
-                                          make_column_selector(dtype_include=np.float))],
+                                          make_column_selector(dtype_include=np.floating))],
                                         remainder="passthrough")
         logistic = LogisticRegressionCV(class_weight="balanced",
                                         scoring="roc_auc",
@@ -177,7 +177,7 @@ class SurvivalModel:
         self.max_features_to_select = max_features_to_select
         self.n_jobs = n_jobs
         self.transformer = ColumnTransformer([('scale', StandardScaler(),
-                                                make_column_selector(dtype_include=np.float))],
+                                                make_column_selector(dtype_include=np.floating))],
                                              remainder="passthrough")
                                                # ('onehot',
                                                #  OneHotEncoder(drop="first", sparse=False),
@@ -225,7 +225,7 @@ class SurvivalModel:
         death = X["death"]
         columns = X.columns.drop("death")
         X_transformed = self.transformer.fit_transform(X.drop("death", axis=1))
-        X_transformed = pd.DataFrame(X, index=y.index, columns=columns)
+        X_transformed = pd.DataFrame(X_transformed, index=y.index, columns=columns)
         X_transformed["death"] = death
         self.model.fit(X_transformed, y)
         return self

radcurecompetition/baselines/simple/run.py
@@ -83,14 +83,14 @@ def main(args):
         true, pred = baseline.get_test_predictions()
         results_binary = evaluate_binary(true["binary"],
                                          pred["binary"],
-                                         args.n_permutations,
-                                         args.n_jobs)
+                                         n_permutations=args.n_permutations,
+                                         n_jobs=args.n_jobs)
         results_survival = evaluate_survival(true["survival_event"],
                                              true["survival_time"],
                                              pred["survival_event"],
                                              pred["survival_time"],
-                                             args.n_permutations,
-                                             args.n_jobs)
+                                             n_permutations=args.n_permutations,
+                                             n_jobs=args.n_jobs)
         results.append({"name": name, **results_binary, **results_survival})
         binary_predictions.append({"name": name, "true": true["binary"], "pred": pred["binary"][:, 1]})
 
@@ -111,6 +111,7 @@ def main(args):
             ax[1].set_xlabel("Recall")
             ax[1].set_ylabel("Precision")
             ax[1].set_title("Precision-recall curves")
+            np.save(f"pred_{p['name']}.npy", p["pred"], pred)
         fig.legend()
         fig.savefig(args.output_path.replace(".csv", ".png"), dpi=300)
 