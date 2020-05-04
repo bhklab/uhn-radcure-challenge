deee1a430c3d4009156675ef6ea96fbe7374006a
mkazmier <mkazmierski.poznan@gmail.com>
2020-05-04 14:37:17 -0400

    [baselines/cnn] Model now runs evaluation on validation set

--- radcurechallenge/baselines/cnn/model.py
+++ radcurechallenge/baselines/cnn/model.py
@@ -1,33 +1,27 @@
 from math import floor
+from argparse import ArgumentParser
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader, random_split
-from torch.nn.optim import Adam
+from torch.utils.data import DataLoader, Subset
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torchvision.transforms import Compose
 
 from sklearn.metrics import roc_auc_score, average_precision_score
+from sklearn.model_selection import train_test_split
 
 from .dataset import RadcureDataset
 from .transforms import *
 
 
 class SimpleCNN(pl.LightningModule):
-    def __init__(self,
-                 hparams,
-                 root_directory,
-                 clinical_data_path,
-                 cache_dir="./.cache",
-                 num_workers=1):
+    def __init__(self, hparams):
         super().__init__()
 
         self.hparams = hparams
-        self.root_directory = root_directory
-        self.clinical_data_path = clinical_data_path
-        self.cache_dir = cache_dir
-        self.num_workers = num_workers
 
         self.conv1 = nn.Conv3d(in_channels=1, out_channels=64, kernel_size=5)
         self.conv2 = nn.Conv3d(in_channels=64, out_channels=128, kernel_size=3)
@@ -84,14 +78,16 @@ class SimpleCNN(pl.LightningModule):
             nn.init.xavier_uniform_(m.bias)
 
     def prepare_data(self):
-        dataset = RadcureDataset(self.root_directory,
-                                 self.clinical_data_path,
-                                 self.hparams.patch_size,
-                                 cache_dir=self.cache_dir)
-
-        test_size = floor(.1 / .7 * len(dataset)) # use 10% of all data for tuning
-        train_dataset, tune_dataset = random_split(dataset, [len(dataset) - test_size, test_size])
-        transform = Compose([
+        train_dataset = RadcureDataset(self.hparams.root_directory,
+                                       self.hparams.clinical_data_path,
+                                       self.hparams.patch_size,
+                                       train=True,
+                                       cache_dir=self.hparams.cache_dir)
+        valid_dataset = RadcureDataset(self.hparams.root_directory,
+                                      self.hparams.clinical_data_path,
+                                      self.hparams.patch_size,
+                                      train=False,
+                                      cache_dir=None)
         # if self.hparams.cache_dir:
             # # load data into cache before training
             # dl = DataLoader(dataset,
@@ -100,35 +96,59 @@ class SimpleCNN(pl.LightningModule):
             # # iterate over dataloader to load data into cache
             # for _ in dl:
             #     continue
+
+        # make sure the tuning set is balanced
+        tune_size = floor(.1 / .7 * len(train_dataset)) # use 10% of all data for tuning
+        train_indices = range(len(train_dataset))
+        train_targets = train_dataset.clinical_data["target_binary"]
+        train_indices, tune_indices = train_test_split(train_indices, test_size=tune_size, stratify=train_targets)
+        train_dataset, tune_dataset = Subset(train_dataset, train_indices), Subset(train_dataset, tune_indices)
+
+        # apply data augmentation only on training set
+        train_transform = Compose([
             ToTensor(),
             Normalize(self.hparams.dataset_mean, self.hparams.dataset_std),
             Random90DegRotation(),
             RandomFlip((0, 1)),
             RandomFlip((0, 2)),
             RandomFlip((1, 2)),
-            RandomNoise(.1)
+            RandomNoise()
         ])
-        train_dataset.transform = transform # only augment the training set
+        valid_transform = Compose([
+            ToTensor(),
+            Normalize(self.hparams.dataset_mean, self.hparams.dataset_std)
+        ])
+        train_dataset.dataset.transform = train_transform
+        tune_dataset.dataset.transform = valid_transform
+        valid_dataset.transform = valid_transform
         self.train_dataset = train_dataset
         self.tune_dataset = tune_dataset
+        self.valid_dataset = valid_dataset
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset,
                           batch_size=self.hparams.batch_size,
-                          num_workers=self.num_workers,
+                          num_workers=self.hparams.num_workers,
                           shuffle=True)
 
     def val_dataloader(self):
         return DataLoader(self.tune_dataset,
                           batch_size=self.hparams.batch_size,
-                          num_workers=self.num_workers,
+                          num_workers=self.hparams.num_workers,
+                          shuffle=False)
+
+    def test_dataloader(self):
+        return DataLoader(self.valid_dataset,
+                          batch_size=self.hparams.batch_size,
+                          num_workers=self.hparams.num_workers,
                           shuffle=False)
 
     def configure_optimizers(self):
-        adam = Adam(self.parameters(),
-                    lr=self.hparams.learning_rate,
-                    weight_decay=self.hparams.weight_decay)
-        return adam
+        optimizer = Adam(self.parameters(),
+                         lr=self.hparams.lr,
+                         weight_decay=self.hparams.weight_decay)
+        scheduler = ReduceLROnPlateau(optimizer, 'min')
+        return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -156,3 +176,45 @@ class SimpleCNN(pl.LightningModule):
         # log loss and metrics to Tensorboard
         log = {"tuning_loss": loss, "roc_auc": roc_auc, "average_precision": avg_prec}
         return {"tuning_loss": loss, "log": log}
+
+    def test_step(self, batch, batch_idx):
+        return self.validation_step(batch, batch_idx)
+
+    def test_epoch_end(self, outputs):
+        return self.validation_epoch_end(outputs)
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument("--batch_size",
+                            type=int,
+                            default=16,
+                            help="The batch size.")
+        parser.add_argument("--lr",
+                            type=float,
+                            default=3e-4,
+                            help="The initial learning rate.")
+        parser.add_argument("--weight_decay",
+                            type=float,
+                            default=1e-5,
+                            help="The amount of weight decay to use.")
+        parser.add_argument("--patch_size",
+                            type=int,
+                            default=50,
+                            help=("Size of the image patch extracted around"
+                                  "each tumour."))
+        parser.add_argument("--dataset_mean",
+                            type=float,
+                            default=0., # XXX add correct value
+                            help=("The mean pixel intensity used for"
+                                  "input normalization."))
+        parser.add_argument("--dataset_std",
+                            type=float,
+                            default=1., # XXX add correct value
+                            help=("The standard deviation of  pixel intensity"
+                                  "used for input normalization."))
+        parser.add_argument("--pos_weight",
+                            type=float,
+                            default=1e-5, # XXX add corect value
+                            help="The loss weight for positive class.")
+        return parser
--- radcurechallenge/baselines/cnn/train.py
+++ radcurechallenge/baselines/cnn/train.py
@@ -7,23 +7,19 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from .model import SimpleCNN
 
 
-def main(args):
+def main(hparams):
     # XXX uncomment before release
     # if args.gpus == 0:
     #     raise ValueError(("Training on CPU is not supported, please run again "
     #                       "on a system with GPU and '--gpus' > 0."))
 
-    checkpoint_path = os.path.join(args.checkpoint_dir,
+    checkpoint_path = os.path.join(hparams.checkpoint_dir,
                                    "simplecnn_{epoch:02d}-{tune_loss:.2f}-{roc_auc:.2f}")
     checkpoint_callback = ModelCheckpoint(filepath=checkpoint_path,
                                           save_top_k=5,
                                           monitor="tuning_loss")
-    model = SimpleCNN(args, args.clinical_data_path, args.cache_dir, args.num_workers)
-    trainer = Trainer(max_epochs=args.max_epochs,
-                      weights_save_path=args.weights_save_path,
-                      gpus=args.gpus,
-                      fast_dev_run=args.fast_dev_run, # XXX remove before release
-                      checkpoint_callback=checkpoint_callback)
+    model = SimpleCNN(hparams)
+    trainer = Trainer.from_argparse_args(hparams, checkpoint_callback=checkpoint_callback)
     trainer.fit(model)
 
 
@@ -39,18 +35,6 @@ if __name__ == "__main__":
                         type=str,
                         default="./.model_checkpoints",
                         help="Path to CSV file containing the clinical data.")
-    parser.add_argument("--batch_size",
-                        type=int,
-                        default=16,
-                        help="The batch size.")
-    parser.add_argument("--lr",
-                        type=float,
-                        default=3e-4,
-                        help="The initial learning rate.")
-    parser.add_argument("--patch_size",
-                        type=int,
-                        default=50,
-                        help="Size of the image patch extracted around each tumour.")
     parser.add_argument("--checkpoint_dir",
                         type=str,
                         default="./.model_checkpoints",
@@ -61,12 +45,14 @@ if __name__ == "__main__":
                         default=None,
                         help=("Directory where the preprocessed data "
                               "will be saved. If not specified, data will not "
-                              "be cached, which slows down loading but conserves "
+                              "be cached, which slows down loading but saves "
                               "disk space."))
     parser.add_argument("--num_workers",
                         type=int,
                         default=1,
                         help="Number of worker processes to use for data loading.")
 
-    args = parser.parse_args()
-    main(args)
+    parser = SimpleCNN.add_model_specific_args(parser)
+
+    hparams = parser.parse_args()
+    main(hparams)
