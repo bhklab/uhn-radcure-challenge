7ba00c7aa83cb758191159803ef24ff24d41927e
mkazmier <mkazmierski.poznan@gmail.com>
2020-05-11 12:36:18 -0400

    [baselines/cnn] Added prediction script

--- radcurechallenge/baselines/cnn/predict.py
+++ radcurechallenge/baselines/cnn/predict.py
@@ -1,9 +1,41 @@
+from argparse import ArgumentParser
+
+import numpy as np
+import torch
+from pytorch_lightning import Trainer
+
 from .model import SimpleCNN
-from .dataset import RadcureDataset
+
+np.random.seed(42)
+torch.manual_seed(42)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 
-def predict(root_directory, weights_path):
-    model = ""
-    dataset = RadcureDataset(root_directory=root_directory,
-                             clinical_data_path=clinical_data_path,
-                             patch_size=)
+def main(args):
+    # if hparams.gpus == 0:
+    #     raise ValueError(("Training on CPU is not supported, please run again "
+    #                       "on a system with GPU and '--gpus' > 0."))
+
+    model = SimpleCNN.load_from_checkpoint(args.checkpoint_path)
+    print(model)
+    print(model.hparams)
+    model.prepare_data()
+    trainer = Trainer.from_argparse_args(model.hparams)
+    trainer.test(model)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser = Trainer.add_argparse_args(parser)
+
+    parser.add_argument("checkpoint_path",
+                        type=str,
+                        help="Path to saved model checkpoint.")
+    parser.add_argument("--pred_save_path",
+                        type=str,
+                        default="./data/predictions/baseline_cnn.csv",
+                        help="Directory where final predictions will be saved.")
+
+    args = parser.parse_args()
+    main(args)
